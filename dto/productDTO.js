838cf91c549cf33b6a78385ae6da7c86194872e8
Germán Stevssel <gstevssel@Air-de-German.fibertel.com.ar>
2022-05-31 20:15:04 -0300

    elaboración DTO de productos

--- dto/productDTO.js
+++ dto/productDTO.js
@@ -1,10 +1,18 @@
-export default class ProductsForIndexDTO {
+export default class ProductDTO {
 	constructor(product) {
 		this.name = product.name;
 		this.id = product.id;
 		this.price = product.price;
 		this.photo = product.photo;
 	}
+
+	getId = () => this.id;
+
+	getName = () => this.name;
+
+	getPrice = () => this.price;
+
+	getPhoto = () => this.photo;
 
 	product = async () => this;
 }
